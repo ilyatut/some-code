ddc0a275f85389973e2243e45f7571c34c3603b0
Ilya Tyumentsev <ilyatut@gmail.com>
2021-10-16 01:05:02 +0500

    Early checks for a file existance

--- public_html/count_ips.php
+++ public_html/count_ips.php
@@ -27,33 +27,33 @@ function readFileLines(string $filename)
  */
 function getMostFrequentIp(string $fileName)
 {
-    $uniqueIpsList = [];
-
-    if (file_exists($fileName)) {
-
-        # read the file line by line
-        foreach (readFileLines($fileName) as $line) {
-
-            # get IP from the string
-            [ , , $ipString ] = explode(' ', $line);
-
-            # convert IP string to a number for memory efficiency
-            $ipLong = ip2long($ipString);
-            if ($ipLong) {
-                $uniqueIpsList[ $ipLong ]++;
-            }
-        }
-
-        # sort the list
-        arsort($uniqueIpsList);
-
-        # take the first element
-        $firstKey = array_key_first($uniqueIpsList);
-
-        # convert the IP back to the string
-        return long2ip($firstKey);
+    # check if the file exists
+    if (!file_exists($fileName)) {
+        throw new RuntimeException('File with IPs not found');
     }
-    throw new RuntimeException('File with IPs not found');
+
+    # read the file line by line
+    $uniqueIpsList = [];
+    foreach (readFileLines($fileName) as $line) {
+
+        # get IP from the string
+        [ , , $ipString ] = explode(' ', $line);
+
+        # convert IP string to a number for memory efficiency
+        $ipLong = ip2long($ipString);
+        if ($ipLong) {
+            $uniqueIpsList[ $ipLong ]++;
+        }
+    }
+
+    # sort the list
+    arsort($uniqueIpsList);
+
+    # take the first element
+    $firstKey = array_key_first($uniqueIpsList);
+
+    # convert the IP back to the string
+    return long2ip($firstKey);
 }
 
 /**
@@ -66,34 +66,34 @@ function getMostFrequentIp(string $fileName)
  */
 function getMostFrequentUrls(string $fileName, string $ip, int $top = 10)
 {
-    $uniqueUrlsList = [];
-
-    if (file_exists($fileName)) {
-
-        # read the file line by line
-        foreach (readFileLines($fileName) as $line) {
-
-            # get IP and URL from the string
-            [ , , $ipString, $urlString ] = explode(' ', $line);
-
-            # convert IP string to a number for memory efficiency
-            if ($ipString === $ip) {
-                $uniqueUrlsList[ $urlString ]++;
-            }
-        }
-
-        # sort the list
-        arsort($uniqueUrlsList);
-
-        # get the top
-        if ($top) {
-            $uniqueUrlsList = array_slice($uniqueUrlsList, 0, $top);
-        }
-
-        # convert the IP back to the string
-        return $uniqueUrlsList;
+    # check if the file exists
+    if (!file_exists($fileName)) {
+        throw new RuntimeException('File with IPs not found');
     }
-    throw new RuntimeException('File with IPs not found');
+
+    # read the file line by line
+    $uniqueUrlsList = [];
+    foreach (readFileLines($fileName) as $line) {
+
+        # get IP and URL from the string
+        [ , , $ipString, $urlString ] = explode(' ', $line);
+
+        # convert IP string to a number for memory efficiency
+        if ($ipString === $ip) {
+            $uniqueUrlsList[ $urlString ]++;
+        }
+    }
+
+    # sort the list
+    arsort($uniqueUrlsList);
+
+    # get the top elements
+    if ($top) {
+        $uniqueUrlsList = array_slice($uniqueUrlsList, 0, $top);
+    }
+
+    # convert the IP back to the string
+    return $uniqueUrlsList;
 }
 
 
